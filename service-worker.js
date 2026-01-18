258f1671aea7eb5218fd8583a00064cf71a13c88
taverna-da-osvaldo <giakipando@gmail.com>
2026-01-18 11:02:12 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open("consegne-cache").then(cache =>
+cache.addAll([
+"./",
+"./consegne.html"
+])
+)
+);
+});
+
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(response =>
+response || fetch(e.request)
+)
+);
+});
